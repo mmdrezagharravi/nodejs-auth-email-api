9ffde0c9022b21680f5f318ad77e4a8402baafca
mohammadreza8410 <mohammadrezagharravi2002@gmail.com>
2025-12-11 12:29:36 +0330

    fix pushe

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
-required("dotenv").config();
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const authRouter = require("./controllers/authControler");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/api/auth", authRouter);
+app.use("/api/auth", authRoutes);
 
 mongoose
   .connect(process.env.MONGODB_URI)
-  .then(() => console.log("connected to mongo "))
-  .catch((err) => console.log("error : ", err));
+  .then(() => console.log("✅ متصل به MongoDB"))
+  .catch((err) => console.log("❌ خطا:", err));
 
 const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => {
-  console.log(`connected on port ${PORT}`);
+  console.log(`🚀 سرور روی پورت ${PORT} اجرا شد`);
 });
